b0e0a4358f2aeb666d50cb5539880c4defc910d3
Souhaiel <karbaa.souheil@gmail.com>
2024-01-10 14:12:18 +0900

    fix duplicate rendering when refreshing

--- index.js
+++ index.js
@@ -1,94 +1,107 @@
 const inputField = document.getElementById("input");
 const addBtn = document.getElementById("addBtn");
-
 const tasksContainer = document.querySelector(".tasks-container");
 
 
-
 let tasks = [
-
     //here goes the data for each task
-]
+];
+
+tasks = JSON.parse(localStorage.getItem("tasks"))
+
 
 function displayNoTasksMessage() {
-const noTasksMessage = document.querySelector(".no-tasks-message");
-    if (tasks.length > 0) {
-        noTasksMessage.style.display = "none";
-        
-    }
-    else if (tasks.length === 0) {
-        noTasksMessage.style.display = "block";
-      
-    }
-
+  const noTasksMessage = document.querySelector(".no-tasks-message");
+  if (tasks.length > 0) {
+    noTasksMessage.style.display = "none";
+  } else if (tasks.length === 0) {
+    noTasksMessage.style.display = "block";
+  }
 }
 
-function deleteTask(index){
-    if (confirm(`Are you sure you want to delete this task? "${tasks[index].text}"`)) {
-    tasks.splice(index, 1); 
+function deleteTask(index) {
+  if (
+    confirm(`Are you sure you want to delete this task? "${tasks[index].text}"`)
+  ) {
+    tasks.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
     tasksContainer.innerHTML = "";
     fillTasksContent();
-    displayNoTasksMessage()
-    console.log(tasks.length)}
-  
-}
-function editTask(index){
-newText = prompt("Enter new task", tasks[index].text);
-tasks[index].text = newText;
-tasksContainer.innerHTML = "";
-fillTasksContent();  
-}
-
-function toggleCompletionStatus(index){
-    const task = document.querySelector(".task");
-    tasks[index].completed = !tasks[index].completed;
-    tasksContainer.innerHTML = "";
-    fillTasksContent();
+    displayNoTasksMessage();
+  }
 }
 
 
+
+function editTask(index) {
+  newText = prompt("Enter new task", tasks[index].text);
+  tasks[index].text = newText;
+  tasksContainer.innerHTML = "";
+  fillTasksContent();
+}
+
+function toggleCompletionStatus(index) {
+  const task = document.querySelector(".task");
+  tasks[index].completed = !tasks[index].completed;
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  tasksContainer.innerHTML = "";
+  fillTasksContent();
+}
 
 function fillTasksContent() {
-    let index = 0;
-    tasks.forEach((task) => {
-        
-        tasksContainer.innerHTML += ` <div class="task" style="background: ${task.completed ? "#a7f1a7" : null}">
+  let index = 0;
+  tasks.forEach((task) => {
+    tasksContainer.innerHTML += ` <div class="task" style="background: ${
+      task.completed ? "#a7f1a7" : null
+    }">
         <div class="task-details">
-            <h3 style="text-decoration: ${task.completed ? "Line-through" : null}">${task.text}</h3>
-            <p style="text-decoration: ${task.completed ? "Line-through" : null}">${task.date}</p>
+            <h3 style="text-decoration: ${
+              task.completed ? "Line-through" : null
+            }">${task.text}</h3>
+            <p style="text-decoration: ${
+              task.completed ? "Line-through" : null
+            }">${task.date}</p>
         </div>
         <div class="action-buttons">
-            <span onclick="toggleCompletionStatus(${index})" id="completed" class="material-symbols-outlined">${!task.completed ? "done" : "cancel"}</span>
+            <span onclick="toggleCompletionStatus(${index})" id="completed" class="material-symbols-outlined">${
+      !task.completed ? "done" : "cancel"
+    }</span>
             <span onclick="deleteTask(${index})" id="delete" class="material-symbols-outlined">delete</span>
             <span onclick="editTask(${index})" id="edit" class="material-symbols-outlined edit">edit</span>
         </div>
-        </div>`
-       index++; 
-    })
-    displayNoTasksMessage()
+        </div>`;
+    index++;
+  });
+
+  displayNoTasksMessage();
 }
-
-
-
-
 
 fillTasksContent();
 
+addBtn.addEventListener("click", function () {
+  tasksContainer.innerHTML = "";
+  const now = new Date();
+  if (inputField.value.length > 0) {
+    tasks.push({
+      text: inputField.value,
+      date:
+        now.getDate() +
+        "/" +
+        (now.getMonth() + 1) +
+        "/" +
+        now.getFullYear() +
+        " | " +
+        now.getHours() +
+        ":" +
+        now.getMinutes(),
+      isCompleted: false,
+    });
+
+    inputField.value = "";
+  }
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  fillTasksContent();
+});
 
 
-addBtn.addEventListener("click",function() {
-tasksContainer.innerHTML = "";
-const now = new Date();
-    if(inputField.value.length > 0 ) {
-        tasks.push({
-            text: inputField.value,
-            date:  now.getDate() + "/" + (now.getMonth() + 1) + "/" +  now.getFullYear()+ " | " + now.getHours() + ":" + now.getMinutes(),
-            isCompleted: false
-        })
-        
-        inputField.value = "";}
-        fillTasksContent();
-        console.log(tasks)
-}
-)
-fillTasksContent();
+
